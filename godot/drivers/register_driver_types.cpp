6d92d99fea2c1ff7dd2290ae0c2afb3da7911229
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-03-08 16:12:48 -0800

    Update RenderingNativeSurface documentation.

--- godot/drivers/register_driver_types.cpp
+++ godot/drivers/register_driver_types.cpp
@@ -44,12 +44,6 @@ static Ref<ResourceSaverPNG> resource_saver_png;
 
 void register_core_driver_types() {
 	GDREGISTER_ABSTRACT_CLASS(RenderingNativeSurface)
-#ifdef PLATFORM_APPLE
-	GDREGISTER_ABSTRACT_CLASS(RenderingNativeSurfaceApple)
-#endif
-#ifdef VULKAN_ENABLED
-	GDREGISTER_ABSTRACT_CLASS(RenderingNativeSurfaceVulkan)
-#endif
 
 	image_loader_png.instantiate();
 	ImageLoader::add_image_format_loader(image_loader_png);
